1986b9598ab854b9455172824ff86f5a5e13539d
Amin Aarabi <arabi.amin69@outlook.com>
2023-02-23 22:04:28 +0330

    Create News Entites

---
+++ News.Entites/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Entites
+{
+    public class Category:BaseEntity<Byte>
+    {
+        public byte? ParentId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ News.Entites/News.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Entites
+{
+    public enum NewsState
+    {
+        Draft,
+        Publish,
+        Delete
+    }
+    public class News : BaseEntity<int>
+    {
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Body { get; set; }
+        public List<Keyword> Keywords { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<NewsFile> Attachments { get; set; }
+        public byte MainCategoryId { get; set; }
+        public int MainFileId { get; set; }
+        public NewsState State { get; set; }
+
+    }
+}
